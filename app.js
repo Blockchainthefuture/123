c0688c5b7148e68a82903f51c4d944626ce21393
y77cao <y77cao@uwaterloo.ca>
2016-11-19 13:03:58 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var user = require('./routes/user');
+app.get('/users', user.list);
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+var app = express();
+
+try {
+  var configJSON = fs.readFileSync(__dirname + "/config.json");
+  var config = JSON.parse(configJSON.toString());
+} catch (e) {
+  console.error("File config.json not found or is invalid: " + e.message);
+  process.exit(1);
+}
+routes.init(config);
+
+app.set('port', config.port || process.env.PORT);
+app.set('views', __dirname + '/views');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
